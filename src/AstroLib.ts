55075fb5d4affbd5c8bf109055f058561678e8b2
Paul Harrison <paul.harrison@manchester.ac.uk>
2024-08-15 16:42:40 +0100

    some initial config

---
+++ src/AstroLib.ts
@@ -0,0 +1,7 @@
+import { TSOFA  } from "@tsastro/tsofa";
+
+// not sure what the best library structure is https://www.typescriptlang.org/docs/handbook/declaration-files/library-structures.html
+
+declare namespace AstroLib {
+
+}
